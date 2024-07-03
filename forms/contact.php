10600460db0762bd4000c4d64b3747ac956e0cd5
Andres Felipe Portilla Amorocho <156432253+Andre230906@users.noreply.github.com>
2024-07-03 13:38:16 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,9 @@
 <?php
-// Obtener los datos del formulario
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-// Crear un array con los datos del formulario
 $formData = array(
     'name' => $name,
     'email' => $email,
@@ -13,20 +11,15 @@ $formData = array(
     'message' => $message
 );
 
-// Obtener el contenido actual del archivo JSON
 $jsonFile = file_get_contents('datos.json');
 $data = json_decode($jsonFile, true);
 
-// Agregar los nuevos datos al array existente
 $data[] = $formData;
 
-// Convertir el array a formato JSON
 $jsonData = json_encode($data, JSON_PRETTY_PRINT);
 
-// Guardar los datos en el archivo JSON
 file_put_contents('datos.json', $jsonData);
 
-// Enviar una respuesta al cliente
 echo "¡Datos almacenados correctamente en el archivo JSON!";
 ?>
 
